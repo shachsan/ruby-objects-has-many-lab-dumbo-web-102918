0a9f32bc8ab8ad7840d751b9866556c69f342913
SANJAY K SAI <shachsan@hotmail.com>
2018-11-01 20:58:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,9 @@ class Song
   end
 
   def artist_name
-    artist_name = self.artist.name
     
+    artist_name = self.artist.name
+
     artist_name.empty? ? nil : artist_name
       # binding.pry
   end
